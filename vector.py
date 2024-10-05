81d1a1ef7fd3c75e727001993a6fbebc5b0e54f8
eladkapuza <eladkapuza@gmail.com>
2024-10-05 22:25:25 +0300

    updated vector

--- vector.py
+++ vector.py
@@ -1,36 +1,44 @@
-import math
-
-
 class Vector:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+    def __init__(self, size=0):
+        if size == 0:
+            self._arr = []
+        else:
+            self._arr = [0] * size
 
     def __str__(self):
-        return f'({self.x},{self.y},{self.z})'
+        return '[' + ' '.join(str(x) for x in self._arr) + ']'
 
     def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+        v = Vector()
+        for x in self._arr + other._arr:
+            v._arr.append(x)
+        return v
 
     def __sub__(self, other):
-        return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+        pass
 
     def __pow__(self, power):
-        return Vector(self.x ** power, self.y ** power, self.z ** power)
+        B = Vector(len(self._arr))
+        for i in range(len(self._arr)):
+            B._arr[i] = self._arr[i] ** power
+        return B
+
+    def __iadd__(self, x):
+        self._arr.append(x)
+        return self
 
     def __cmp__(self, other):
-        return self.x == other.x and self.y == other.y and self.z == other.z
+        pass
 
 
 if __name__ == '__main__':
-    v1 = Vector(1, 2, 3)
-    v2 = Vector(2, 4, 6)
-    v3 = v1 + v2
-    print(v3)
+    A = Vector()
+    B = Vector(size=8)
 
-    v4 = v1 - v2
-    print(v4)
+    for x in range(1, 11):
+        A += x
 
-    s1 = v1 ** 2
-    print(s1)
+    print(A)
+    A2 = A ** 2
+
+    print(A2)
